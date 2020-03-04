0f6face438ab4e283777a44364c0de2cd321cce5
李焕健 <0004904@suiyi.com.cn>
2020-03-04 10:23:07 +0800

    fix：修复“分包、ssr”失败问题

--- server/routes/viewRoute.js
+++ server/routes/viewRoute.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
-import { StaticRouter,matchPath  } from 'react-router'
-import {Provider} from 'react-redux'
+import { StaticRouter } from 'react-router'
+import { Provider } from 'react-redux'
 import configureStore from '../../client/redux/configureStore'
 import App from '../../client/App'
-import routeConfig from "../../client/routeConfig";
+import { matchComp } from "../../client/routeConfig";
 
 
 
@@ -15,10 +15,14 @@ const store = configureStore()
 async function viewRoute(ctx, next) {
 
     // 因为所有请求，不单单是页面请求，包括图片请求都会进入本方法，必须判断该url是否匹配上页面路由
-    if(routeConfig.isMatch(ctx.url)){
+    const comp = matchComp(ctx.url)
+    if(comp){
+        if (process.env.NODE_ENV === "production" && comp.preload) {
+            await comp.preload()
+        }
         await ctx.render('index', {
             // 渲染dom有多种方法，这里只最简单的[renderToString]
-            root: await ReactDOMServer.renderToString(
+            root: ReactDOMServer.renderToString(
                 <Provider store={store}>
                     <StaticRouter
                         location={ctx.url}
